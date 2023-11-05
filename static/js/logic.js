5b87d999faa36cf769720caf39381d90713ffaef
Caleb_Wolf <cwolf1424@gmail.com>
2023-11-05 15:56:40 -0600

    Topo Map Added

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,20 +5,35 @@ d3.json(url).then(function(data){
     console.log (data)
 });
 
+//Set tile layer variables
+let streetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'});
+
+let topoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+
+//Set basemaps
+let baseMaps = {
+    "Street Map": streetMap,
+    "Topographical Map": topoMap
+};
+
 //Set map at default view around KCK
-let myMap = L.map("map").setView([39.106667, -94.676392], 5);
+let myMap = L.map("map", {
+    center: [39.106667,-94.676392],
+    zoom: 5,
+    layers: [streetMap]
+});
 
-//Set tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-     {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(myMap);
-
+//Set Control Layer
+L.control.layers(baseMaps).addTo(myMap);
 
 //Set circle around KCK 
 let circle = L.circle(
-    
+
     [39.106667,-94.676392],
-    
-    {radius: 10000,
+    {radius: 100000,
     color: "black",
     fillColor: "white",
     weight:0.5,
